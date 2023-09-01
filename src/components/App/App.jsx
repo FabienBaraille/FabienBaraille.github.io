b7a7100c02f8ec5111f3958178d79b9978809e3c
FabienBaraille <fabien.baraille@oclock.school>
2023-09-01 13:06:25 +0200

    React version

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import logo from '../../assets/logo.svg';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Header</div>
+      <div>Main</div>
+      <div>Footer</div>
+    </div>
+  );
+}
+
+export default App;
