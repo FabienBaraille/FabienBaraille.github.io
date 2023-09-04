43c6053280b54d1d789a6ccf6559717ced3c3db3
FabienBaraille <fabien.baraille@oclock.school>
2023-09-04 15:05:05 +0200

    mise en place

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header> Dev Web</header>
+      <body>
+        a faire
+      </body>
+      <footer>
+        contact
+      </footer>
+    </div>
+  );
+}
+
+export default App;
